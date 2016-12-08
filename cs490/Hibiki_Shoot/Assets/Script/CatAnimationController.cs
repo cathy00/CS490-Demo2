913b940d64cf3cdd5a72ce9b3d4b0f60add8cd86
sine27 <feng87@purdue.edu>
2016-12-08 16:04:48 -0500

    Enable : Go some Position and Eat

---
+++ cs490/Hibiki_Shoot/Assets/Script/CatAnimationController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAnimationController : MonoBehaviour {
+
+	public Animator cat_animator;
+	public GameObject target;
+	public bool move = false;
+
+	// Use this for initialization
+	void Start () {
+		cat_animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float secondX = target.transform.position.x;
+		float secondy = transform.position.y;
+		float secondz = target.transform.position.z;
+		Vector3 secondPosition = new Vector3(secondX, secondy, secondz);
+
+		if (Input.GetKeyDown ("1")) {
+			goPosition ();
+		}
+
+		if ( move ) {
+			Debug.Log ("CatAnimationController : Before : " + transform.position + " " + secondPosition); 
+			transform.LookAt (secondPosition);
+			cat_animator.Play ("go_position", -1);
+			Debug.Log ("CatAnimationController : After : " + transform.position);
+		}
+
+//		Debug.Log ("CatAnimationController : Distance : " +  Vector3.Distance(transform.position, secondPosition));
+
+		if (move && Vector3.Distance(transform.position, secondPosition) <= 2.5f) {			
+			move = false;
+			StartCoroutine(EndAction ());
+		}
+	}
+
+	void goPosition() {
+		move = true;
+	}
+
+	IEnumerator EndAction()
+	{
+		cat_animator.Play ("eat", -1);
+		yield return new WaitForSeconds(1.5f);
+		target.GetComponent<Renderer>().enabled = false;
+	}
+}
--- cs490/Hibiki_Shoot/Assets/Script/TrotMotion.cs
+++ cs490/Hibiki_Shoot/Assets/Script/TrotMotion.cs
@@ -5,7 +5,7 @@ public class TrotMotion : StateMachineBehaviour {
 
 	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		Debug.Log ("Before: " + animator.transform.position);
+//		Debug.Log ("Before: " + animator.transform.position);
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -17,7 +17,7 @@ public class TrotMotion : StateMachineBehaviour {
 		else if (animator.transform.position.x > 10) {
 			animator.transform.Rotate (0, 0.8f, 0);
 		}
-		else if (animator.transform.position.z > 5) {
+		else if (animator.transform.position.z > 2) {
 			animator.transform.Rotate (0, 0.8f, 0);
 		}
 		else if (animator.transform.position.x < -10) {
@@ -29,16 +29,6 @@ public class TrotMotion : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		Debug.Log ("After: " + animator.transform.position);
+//		Debug.Log ("After: " + animator.transform.position);
 	}
-
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
 }
